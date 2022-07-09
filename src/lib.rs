23cf584f41d30cd9014e0844c5369004ca18b7e9
Michael Volling Jr <mvollingjr@gmail.com>
2022-07-08 22:54:26 -0500

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,10 @@
 
 use chumsky::prelude::*;
 
+/// The Literal type represents an IDL literal value
+///
+/// There is only basic support at the moment, with the wchar
+/// and wstring types not being yet supported.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Literal {
     Bool(bool),
@@ -32,44 +36,82 @@ pub enum Literal {
 }
 
 impl Literal {
+    /// Builds a parser is able to parse a true boolean literal
     pub fn true_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.4.1.3 (19) True values are represented as "TRUE"
         text::keyword("TRUE").map(|_| Literal::Bool(true))
     }
 
+    /// Builds a parser is able to parse a false boolean literal
     pub fn false_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.4.1.3 (19) False values are represented as "FALSE"
         text::keyword("FALSE").map(|_| Literal::Bool(false))
     }
 
+    /// Builds a parser is able to parse any boolean literal
     pub fn bool_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.4.1.3 (19) <boolean_literal> ::= "TRUE" | "FALSE"
         Self::true_parser().or(Self::false_parser())
     }
 
+    /// Builds a parser is able to parse a decimal integer literal
     pub fn dec_int_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.1
+        // An integer literal consisting of a sequence of digits is taken to be decimal
+        // (base ten) unless it begins with 0 (digit zero).
         text::int(10).map(|d: String| Literal::Integer(d.parse().unwrap()))
     }
 
+    /// Builds a parser is able to parse a hex integer literal
     pub fn hex_int_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.1
+        // A sequence of digits preceded by 0x (or 0X) is taken to be a hexadecimal
+        // integer (base sixteen). The hexadecimal digits include a (or A) through
+        // f (or F) with decimal values ten through fifteen, respectively.
         just("0x")
             .or(just("0X"))
             .ignore_then(text::int(16))
             .map(|d: String| Literal::Integer(u64::from_str_radix(d.as_str(), 16).unwrap()))
     }
 
+    /// Builds a parser is able to parse a octal integer literal
     pub fn oct_int_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.1
+        // A sequence of digits starting with 0 is taken to be an octal integer (base eight).
+        // The digits 8 and 9 are not octal digits and thus are not allowed in an octal
+        // integer literal.
         just("0").then(text::int(8)).map(|(_p, d): (&str, String)| {
             Literal::Integer(u64::from_str_radix(d.as_str(), 8).unwrap())
         })
     }
 
+    /// Builds a parser is able to parse any integer literal
     pub fn int_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
-        // Ensure that hex is before oct and that oct is before dec to prevent misreads
+        // 7.2.6.1
+        // An integer literal consisting of a sequence of digits is taken to be decimal
+        // (base ten) unless it begins with 0 (digit zero).
+        //
+        // A sequence of digits starting with 0 is taken to be an octal integer (base eight).
+        // The digits 8 and 9 are not octal digits and thus are not allowed in an octal
+        // integer literal.
+        //
+        // A sequence of digits preceded by 0x (or 0X) is taken to be a hexadecimal
+        // integer (base sixteen). The hexadecimal digits include a (or A) through
+        // f (or F) with decimal values ten through fifteen, respectively.
         Self::hex_int_parser()
             .or(Self::oct_int_parser())
             .or(Self::dec_int_parser())
     }
 
+    /// Builds a parser is able to parse any floating point literal
     pub fn float_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
-        // TODO: Support e notation
+        // 7.2.6.4
+        // A floating-point literal consists of an integer part, a decimal point
+        // (.), a fraction part, an e or E, and an optionally signed integer
+        // exponent. The integer and fraction parts both consist of a sequence
+        // of decimal (base ten) digits. Either the integer part or the fraction
+        // part (but not both) may be missing; either the decimal point or the
+        // letter e (or E) and the exponent (but not both) may be missing.
         let digits = text::digits(10);
         let dot = just('.');
 
@@ -92,7 +134,14 @@ impl Literal {
         decimal_and_fractional.or(decimal_only).or(fractional_only)
     }
 
+    /// Builds a parser is able to parse a fixed point literal
     pub fn fixed_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.5
+        // A fixed-point decimal literal consists of an integer part, a decimal
+        // point (.), a fraction part and a d or D. The integer and fraction
+        // parts both consist of a sequence of decimal (base 10) digits. Either
+        // the integer part or the fraction part (but not both) may be missing;
+        // the decimal point (but not the letter d or D) may be missing.
         let digits = text::digits(10);
         let dot = just('.');
         let the_d = just('d').or(just('D'));
@@ -118,16 +167,56 @@ impl Literal {
         decimal_and_fractional.or(decimal_only).or(fractional_only)
     }
 
+    /// Builds a parser is able to parse a character literal
     pub fn char_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.2
+        // A char is an 8-bit quantity with a numerical value between 0 and 255 (decimal).
+        // The value of a space, alphabetic, digit, or graphic character literal is the
+        // numerical value of the character as defined in the ISO Latin-1 (8859-1)
+        // character set standard (see Table 7-2 on page 14, Table 7-3 on page 15 and Table
+        // 7-4 on page 16). The value of a null is 0. The value of a formatting character
+        // literal is the numerical value of the character as defined in the ISO 646
+        // standard (see Table 7-5 on page 17). The meaning of all other characters is
+        // implementation-dependent.
+        //
+        // NOTE: Since ASCII and ISO 646 are the same for 8 bit characters, we should
+        // be fine to use 'is_ascii'
+        //
         // TODO: Support escape sequences
         filter::<_, _, Simple<char>>(|c: &char| c.is_ascii())
             .delimited_by(just("'"), just("'"))
             .map(|c: char| Self::Character(c))
     }
 
+    /// Builds a parser is able to parse a string literal
     pub fn string_parser() -> impl Parser<char, Literal, Error = Simple<char>> {
+        // 7.2.6.3
+        // Strings are null-terminated sequences of characters. Strings are of
+        // type string if they are made of non-wide characters or wstring
+        // (wide string) if they are made of wide characters.
+        //
+        // A string literal is a sequence of character literals (as defined in
+        // 7.2.6.2, Character Literals), with the exception of the character with
+        // numeric value 0, surrounded by double quotes, as in:
+        //    const string S1 = "Hello";
+        //
+        // Wide string literals have in addition an L prefix, for example:
+        //    const wstring S2 = L"Hello";
+        //
+        // Both wide and non-wide string literals must be specified using characters
+        // from the ISO Latin-1 (8859-1) character set.  A string literal shall not
+        // contain the character ‘\0’. A wide string literal shall not contain the
+        // wide character with value zero.
+        //
+        // Adjacent string literals are concatenated. Characters in concatenated strings
+        // are kept distinct. For example, "\xA" "B" contains the two characters
+        // ‘\xA’ and ‘B’ after concatenation (and not the single hexadecimal character
+        // ‘\xAB’).
+        //
         // TODO: Support escape sequences
-        // FIXME: This should ideally use the Latin-1 character set
+        //
+        // FIXME: Right now we are parsing the utf-8 format. Ideally we would use the
+        // Latin-1 character set
         let single_string = filter::<_, _, Simple<char>>(|c: &char| *c != '"')
             .repeated()
             .delimited_by(just('"'), just('"'))
@@ -140,6 +229,7 @@ impl Literal {
             .map(|vs| Self::Str(vs.concat()))
     }
 
+    /// Builds a parser is able to parse any literal
     pub fn parser() -> impl Parser<char, Literal, Error = Simple<char>> {
         Self::bool_parser()
             .or(Self::fixed_parser()) // Fixed needs to be before float
